4a61480bce851642afce979c2332e8233c184d9d
trongnhanle <letrongnhan1410200@gmail.com>
2021-05-04 23:17:40 +0700

    aa
    aa

--- src/main/java/serverapi/Tables/User/UserConfig.java
+++ src/main/java/serverapi/Tables/User/UserConfig.java
@@ -11,25 +11,25 @@ public class UserConfig {
     @Bean
     User commandLineUsers(UserRepository userRepository){
 
-//        for(int i=0;i<1;i++){
-//            User user = new User();
-//            Scanner sc = new Scanner(System.in);
-//            System.out.println("Nhập username:");
-//            user.setUser_name(sc.nextLine());
-//            System.out.println("Nhập email:");
-//            user.setUser_email(sc.nextLine());
-//            System.out.println("Nhập password:");
-//            user.setUser_password(sc.nextLine());
-//            System.out.println("Thêm avatar:");
-//            user.setUser_avatar(sc.nextLine());
-//            System.out.println("Admin là :");
-//            user.setUser_isAdmin(sc.hasNextBoolean());
-//
-//            userRepository.save(user);
-//            System.out.println("-------------------------------");
-//
-//
-//        }
+        for(int i=0;i<1;i++){
+            User user = new User();
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Nhập username:");
+            user.setUser_name(sc.nextLine());
+            System.out.println("Nhập email:");
+            user.setUser_email(sc.nextLine());
+            System.out.println("Nhập password:");
+            user.setUser_password(sc.nextLine());
+            System.out.println("Thêm avatar:");
+            user.setUser_avatar(sc.nextLine());
+            System.out.println("Admin là :");
+            user.setUser_isAdmin(sc.hasNextBoolean());
+
+            userRepository.save(user);
+            System.out.println("-------------------------------");
+
+
+        }
 
 
 
