67e4fb2ae893aefca52aaabf1a124556cce116f1
MChau105 <bdmchau10005@gmail.com>
2021-05-04 22:57:10 +0700

    ;

--- src/main/java/serverapi/Tables/Manga/MangaConfig.java
+++ src/main/java/serverapi/Tables/Manga/MangaConfig.java
@@ -2,12 +2,7 @@ package serverapi.Tables.Manga;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.Query;
-import serverapi.Tables.Author.Author;
 import serverapi.Tables.Author.AuthorRepository;
-import serverapi.Tables.Chapter.ChapterRepository;
-
-import java.util.*;
 
 @Configuration
 public class MangaConfig {
@@ -42,41 +37,7 @@ public class MangaConfig {
 
 //        Optional<Author> author = authorRepository.findById(7L);
 //
-        for (int i = 0; i < 1; i++) {
-
-
-
-            Scanner sc = new Scanner(System.in);
-            Manga newmanga = new Manga();
-            System.out.println("Nhap ten manga: ");
-            newmanga.setManga_name(sc.nextLine());
-
-            System.out.println("Nhap trang thai: ");
-            newmanga.setStatus(sc.nextLine());
-
-            System.out.println("Nhap mo ta: ");
-            newmanga.setDescription(sc.nextLine());
-
-            System.out.println("Nhap sao: ");
-            newmanga.setStars(sc.nextFloat());
-
-            System.out.println("Nhap view: ");
-            newmanga.setViews(sc.nextInt());
-
-            sc.nextLine();
-            System.out.println("Nhap thumbnail: ");
-            newmanga.setThumbnail(sc.nextLine());
-
-            newmanga.setDate_publication(Calendar.getInstance(TimeZone.getTimeZone("UTC")));
-            newmanga.setCreatedAt(Calendar.getInstance(TimeZone.getTimeZone("UTC")));
-
-
-
-            mangaRepository.save(newmanga);
-            System.out.println("----------------------------");
-        }
-
-
+   
             return null;
         }
 
