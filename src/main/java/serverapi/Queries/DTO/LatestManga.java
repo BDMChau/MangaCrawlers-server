c654cf7c9ff1f5549acca81d7db568df4936a068
MChau105 <bdmchau10005@gmail.com>
2021-05-18 00:24:24 +0700

    Chau_update DTO

--- src/main/java/serverapi/Queries/DTO/LatestManga.java
+++ src/main/java/serverapi/Queries/DTO/LatestManga.java
@@ -1,7 +1,6 @@
 package serverapi.Queries.DTO;
 
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,7 +11,6 @@ import java.util.Calendar;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class LatestManga {
 
 
@@ -23,4 +21,11 @@ public class LatestManga {
     private Long manga_id;
     private String manga_name;
 
+    public LatestManga(Long chapter_id, String chapter_name, Calendar createdAt, Long manga_id, String manga_name) {
+        this.chapter_id = chapter_id;
+        this.chapter_name = chapter_name;
+        this.createdAt = createdAt;
+        this.manga_id = manga_id;
+        this.manga_name = manga_name;
+    }
 }
