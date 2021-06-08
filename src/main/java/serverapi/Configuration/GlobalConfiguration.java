8f630762f37aae56d35cf362560f46e2276f9cb2
MChau105 <bdmchau10005@gmail.com>
2021-06-08 12:08:09 +0700

    Update GlobalConfiguration.java

--- src/main/java/serverapi/Configuration/GlobalConfiguration.java
+++ src/main/java/serverapi/Configuration/GlobalConfiguration.java
@@ -35,14 +35,14 @@ public class GlobalConfiguration {
 
 
     //////////// auto call http every 20 minutes to wake up app on heroku
-    @Scheduled(fixedDelay = 1200000)
-    public void autoCallHttp() throws IOException {
-        URL url = new URL(System.getenv("HOST_PRODUCTION") + "api/auth/autocallhttp");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        con.connect();
-        con.getResponseCode();
-    }
+//    @Scheduled(fixedDelay = 1200000)
+//    public void autoCallHttp() throws IOException {
+//        URL url = new URL(System.getenv("HOST_PRODUCTION") + "api/auth/autocallhttp");
+//        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+//        con.setRequestMethod("GET");
+//        con.connect();
+//        con.getResponseCode();
+//    }
 
 
 
