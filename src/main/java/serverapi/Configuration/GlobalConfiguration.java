4541733bf22f4b092f84826cdad2d2eca512fb20
MChau105 <bdmchau10005@gmail.com>
2021-07-27 02:20:16 +0700

    Chau_ remove auto call heroku

--- src/main/java/serverapi/Configuration/GlobalConfiguration.java
+++ src/main/java/serverapi/Configuration/GlobalConfiguration.java
@@ -7,25 +7,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
-import org.springframework.security.oauth2.client.endpoint.NimbusAuthorizationCodeTokenResponseClient;
-import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
-import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import serverapi.Authentication.RegistrationOauth;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.List;
-import java.util.TimeZone;
 import java.util.stream.Collectors;
 
 @Configuration
@@ -78,14 +68,14 @@ public class GlobalConfiguration {
 
 
     //////////// auto call http every 25 minutes to wake up app on heroku
-    @Scheduled(fixedRate = 1500000)
-    public void autoCallHttp() throws IOException {
-        URL url = new URL(System.getenv("HOST_PRODUCTION") + "api/auth/autocallhttp");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        con.connect();
-        con.getResponseCode();
-    }
+//    @Scheduled(fixedRate = 1500000)
+//    public void autoCallHttp() throws IOException {
+//        URL url = new URL(System.getenv("HOST_PRODUCTION") + "api/auth/autocallhttp");
+//        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+//        con.setRequestMethod("GET");
+//        con.connect();
+//        con.getResponseCode();
+//    }
 
 
 
