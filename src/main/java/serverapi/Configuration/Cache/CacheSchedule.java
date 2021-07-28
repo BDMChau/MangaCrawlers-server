8352f0144c596cff8f920a14c99d15a6c9809428
MChau105 <bdmchau10005@gmail.com>
2021-07-28 22:01:53 +0700

    Update CacheSchedule.java

--- src/main/java/serverapi/Configuration/Cache/CacheSchedule.java
+++ src/main/java/serverapi/Configuration/Cache/CacheSchedule.java
@@ -1,13 +1,8 @@
 package serverapi.Configuration.Cache;
 
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-
-import java.util.Calendar;
 
 
 @Configuration
@@ -16,32 +11,32 @@ import java.util.Calendar;
 public class CacheSchedule {
 
 
-    @Async
-    @CacheEvict(allEntries = true, value = {"weeklyMangas"})
-//    @Scheduled(cron = "0 0 0 */7 * ?")
-    @Scheduled(fixedRate = 604800000)
-    public void evictCacheWeeklyMangas() {
-        Calendar calendar = Calendar.getInstance();
-        System.err.println("Evict Cache weeklyMangas every 7 days: " + calendar.getTime());
-    }
-
-
-    @Async
-    @CacheEvict(allEntries = true, value = {"topMangas"})
-    @Scheduled(fixedRate = 86400000)
-    public void evictCacheTopMangas() {
-        Calendar calendar = Calendar.getInstance();
-        System.err.println("Evict Cache topMangas every 24 hours: " + calendar.getTime());
-    }
-
-
-    @Async
-    @CacheEvict(allEntries = true, value = {"dailyMangas"})
-    @Scheduled(fixedRate = 86400000)
-    public void evictCacheDailyMangas() {
-        Calendar calendar = Calendar.getInstance();
-        System.err.println("Evict Cache dailyMangas every 24 hours: " + calendar.getTime());
-    }
+//    @Async
+//    @CacheEvict(allEntries = true, value = {"weeklyMangas"})
+////    @Scheduled(cron = "0 0 0 */7 * ?")
+//    @Scheduled(fixedRate = 604800000)
+//    public void evictCacheWeeklyMangas() {
+//        Calendar calendar = Calendar.getInstance();
+//        System.err.println("Evict Cache weeklyMangas every 7 days: " + calendar.getTime());
+//    }
+//
+//
+//    @Async
+//    @CacheEvict(allEntries = true, value = {"topMangas"})
+//    @Scheduled(fixedRate = 86400000)
+//    public void evictCacheTopMangas() {
+//        Calendar calendar = Calendar.getInstance();
+//        System.err.println("Evict Cache topMangas every 24 hours: " + calendar.getTime());
+//    }
+//
+//
+//    @Async
+//    @CacheEvict(allEntries = true, value = {"dailyMangas"})
+//    @Scheduled(fixedRate = 86400000)
+//    public void evictCacheDailyMangas() {
+//        Calendar calendar = Calendar.getInstance();
+//        System.err.println("Evict Cache dailyMangas every 24 hours: " + calendar.getTime());
+//    }
 
 
 }
