698652c3f1f84ccc8e93f6cefde17f2f99f1e0b7
MChau105 <bdmchau10005@gmail.com>
2021-09-13 22:35:26 +0700

    Update AuthControllerTest.java

--- src/test/java/serverapi/authentication/AuthControllerTest.java
+++ src/test/java/serverapi/authentication/AuthControllerTest.java
@@ -80,7 +80,6 @@ public class AuthControllerTest {
                         .content(inputJson))
                 .andReturn()
                 .getResponse();
-
         
         assertEquals(200, response.getStatus());
         assertEquals(true, response.getContentAsString().contains("Sign in successfully!"));
