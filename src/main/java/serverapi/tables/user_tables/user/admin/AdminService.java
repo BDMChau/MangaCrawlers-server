242bae634efd4b223b0925822fdc9d9b79b252cf
MChau105 <bdmchau10005@gmail.com>
2021-09-20 16:48:45 +0700

    Update AdminService.java

--- src/main/java/serverapi/tables/user_tables/user/admin/AdminService.java
+++ src/main/java/serverapi/tables/user_tables/user/admin/AdminService.java
@@ -140,7 +140,6 @@ public class AdminService {
             int finalI = i + 1;
             System.err.println("lỗi" + finalI);
             List<Manga> mangaList = new ArrayList<>();
-
             
             mangasInfo.forEach(item -> {
 
