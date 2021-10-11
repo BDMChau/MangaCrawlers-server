dd6bd8709762f634d2bd5db815ed63dd618a488c
MChau105 <bdmchau10005@gmail.com>
2021-10-11 13:24:12 +0700

    Update AuthControllerTest.java

--- src/test/java/serverapi/authentication/AuthControllerTest.java
+++ src/test/java/serverapi/authentication/AuthControllerTest.java
@@ -1,47 +1,21 @@
 package serverapi.authentication;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import serverapi.api.Response;
-import serverapi.authentication.pojo.SignInPojo;
-import serverapi.authentication.service.AuthService;
-import serverapi.authentication.service.Interface.IAuthService;
 import serverapi.helpers.HelpersTest;
-import serverapi.security.HashSHA512;
-import serverapi.tables.user_tables.user.User;
 
-import javax.annotation.Resource;
 import java.util.Map;
-import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(AuthController.class)
@@ -51,12 +25,6 @@ public class AuthControllerTest {
 
     @Autowired
     protected MockMvc mockMvc;
-
-    @MockBean
-    private AuthService authService;
-
-    @MockBean
-    private AuthRepository authRepository;
 
     @Test
     public void testSignIn() throws Exception {
