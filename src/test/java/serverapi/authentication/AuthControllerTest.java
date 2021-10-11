71136e401b31ca0023725b14a15bb5c1c1c1129a
MChau105 <bdmchau10005@gmail.com>
2021-10-11 13:39:17 +0700

    Update AuthControllerTest.java

--- src/test/java/serverapi/authentication/AuthControllerTest.java
+++ src/test/java/serverapi/authentication/AuthControllerTest.java
@@ -53,6 +53,7 @@ public class AuthControllerTest {
     @Test
     public void testSignIn() throws Exception {
         String uri = "/api/auth/signin";
+
         Map<String, String> dataObj = Map.of(
                 "user_email", "bdmchau105@gmail.com",
                 "user_password", "MinhTriet"
@@ -76,7 +77,7 @@ public class AuthControllerTest {
     public void testSignUp() throws Exception {
         String uri = "/api/auth/signup";
 
-        
+
         Map<String, String> dataObj = Map.of(
                 "user_name", "Minh Chou",
                 "user_email", "bdmchau105@gmail.com",
