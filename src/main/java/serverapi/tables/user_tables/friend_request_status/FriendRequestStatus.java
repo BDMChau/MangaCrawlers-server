1aed5fd86ce79dc285feaeae61db99c08c4d0501
Pham Dang Minh Triet <75571286+Gitopham1356@users.noreply.github.com>
2021-10-17 22:11:01 +0700

    aaaa

---
+++ src/main/java/serverapi/tables/user_tables/friend_request_status/FriendRequestStatus.java
@@ -0,0 +1,43 @@
+package serverapi.tables.user_tables.friend_request_status;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import serverapi.tables.user_tables.user.User;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "friend_request_status")
+public class FriendRequestStatus {
+    @Id
+    @SequenceGenerator(
+            name = "friend_request_status_sequence",
+            sequenceName = "friend_request_status_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "friend_request_status_sequence" // same as NAME in SequenceGenerator
+    )
+    private Long friend_request_id;
+
+    @JsonManagedReference
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", insertable = true, updatable = true)
+    private User user;
+
+    @Column(columnDefinition = "boolean default true", nullable = true)
+    private Boolean status;
+
+    @JsonManagedReference
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "to_user_id", insertable = true, updatable = true)
+    private User to_user;
+}
