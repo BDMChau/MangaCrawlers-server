8f404ccc2922d204740e23a4ff69d57ad53eec8b
Pham Dang Minh Triet <75571286+Gitopham1356@users.noreply.github.com>
2021-10-18 15:30:15 +0700

    a

--- src/main/java/serverapi/tables/user_tables/friend_request_status/FriendRequestStatusController.java
+++ src/main/java/serverapi/tables/user_tables/friend_request_status/FriendRequestStatusController.java
@@ -1,4 +1,27 @@
 package serverapi.tables.user_tables.friend_request_status;
 
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletRequest;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/friend_request")
+@CacheConfig(cacheNames = {"friend_request"})
 public class FriendRequestStatusController {
+
+
+//    @PostMapping("/get_friends_user")
+//    public ResponseEntity getFriendsUser(ServletRequest request, @RequestBody Map data) {
+//        String StrUserId = getUserAttribute(request).get("user_id").toString();
+//        Long userId = Long.parseLong(StrUserId);
+//        Integer fromPos = (Integer) data.get("from");
+//
+//        return notificationService.getListNotifications(userId, fromPos);
+//    }
 }
