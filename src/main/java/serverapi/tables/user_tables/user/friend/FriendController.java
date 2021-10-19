677efec19c624117408d6f50abc444acc218fa51
Pham Dang Minh Triet <75571286+Gitopham1356@users.noreply.github.com>
2021-10-20 02:15:38 +0700

    a

--- src/main/java/serverapi/tables/user_tables/user/friend/FriendController.java
+++ src/main/java/serverapi/tables/user_tables/user/friend/FriendController.java
@@ -1,4 +1,27 @@
 package serverapi.tables.user_tables.user.friend;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
 public class FriendController {
+    private final FriendService friendService;
+
+    @Autowired
+    public FriendController(FriendService friendService) {
+        this.friendService = friendService;
+    }
+
+    public Map getUserAttribute(ServletRequest request) {
+        HttpServletRequest req = (HttpServletRequest) request;
+        Map user = (HashMap) req.getAttribute("user");
+        return user;
+    }
+
+
 }
--- src/main/java/serverapi/tables/user_tables/user/friend/FriendService.java
+++ src/main/java/serverapi/tables/user_tables/user/friend/FriendService.java
@@ -1,4 +1,8 @@
 package serverapi.tables.user_tables.user.friend;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class FriendService {
+
 }
