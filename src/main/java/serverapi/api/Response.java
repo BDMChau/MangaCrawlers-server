f5ae5f6c7f1fa49e7546d26fb90f90886c3e2629
MChau105 <bdmchau10005@gmail.com>
2021-11-17 00:55:37 +0700

    Update Response.java

--- src/main/java/serverapi/api/Response.java
+++ src/main/java/serverapi/api/Response.java
@@ -13,33 +13,33 @@ import java.util.HashMap;
 @Data
 @NoArgsConstructor
 public class Response {
-    private int httpCode;
-    private HttpStatus httpStatus;
+    private int http_code;
+    private HttpStatus http_status;
     private Object content;
 
-    public Response(int httpCode, HttpStatus httpStatus, Object content) {
-        this.httpCode = httpCode;
-        this.httpStatus = httpStatus;
+    public Response(int http_code, HttpStatus http_status, Object content) {
+        this.http_code = http_code;
+        this.http_status = http_status;
         this.content = content;
     }
 
     public HashMap toJSON() {
         HashMap hashMap = new HashMap();
-        hashMap.put("http_code", this.httpCode);
-        hashMap.put("http_status", this.httpStatus);
+        hashMap.put("http_code", this.http_code);
+        hashMap.put("http_status", this.http_status);
         hashMap.put("content", this.content);
 
         return hashMap;
     }
 
-    public void ResponseJsonMiddleware(HttpServletResponse res, Gson gson, String contentType, String charSet, int httpCode,
-                                       HttpStatus httpStatus, Object content) throws IOException {
+    public void ResponseJsonMiddleware(HttpServletResponse res, Gson gson, String contentType, String charSet, int http_code,
+                                       HttpStatus http_status, Object content) throws IOException {
         PrintWriter printWriter = res.getWriter();
 
         res.setContentType(contentType);
         res.setCharacterEncoding(charSet);
 
-        Response responseHelper = new Response(httpCode, httpStatus, content);
+        Response responseHelper = new Response(http_code, http_status, content);
         String responseHelperString = gson.toJson(responseHelper);
 
         printWriter.print(responseHelperString);
