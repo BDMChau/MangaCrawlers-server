0c7735f1319d09d4f8ec9a9c20bdc66a1a5da088
Gitopham1356 <75571286+Gitopham1356@users.noreply.github.com>
2021-11-20 18:49:30 +0700

    push

--- src/main/java/serverapi/query/dtos/features/CommentDTOs/CommentDTO.java
+++ src/main/java/serverapi/query/dtos/features/CommentDTOs/CommentDTO.java
@@ -72,11 +72,25 @@ public class CommentDTO {
         this.image_url = image_url;
     }
 
-    public CommentDTO(Long user_id, Long manga_id, Long post_id, Long comment_id) {
+    public CommentDTO(
+            Long user_id, String user_name, String user_avatar,
+            Long manga_id, Long post_id,
+            Long comment_id, Calendar comment_time, String comment_content, int count_like, boolean is_deprecated,
+            Long parent_id,
+            Long comment_image_id, String image_url) {
         this.user_id = user_id;
+        this.user_name = user_name;
+        this.user_avatar = user_avatar;
         this.manga_id = manga_id;
         this.post_id = post_id;
         this.comment_id = comment_id;
+        this.comment_time = comment_time;
+        this.comment_content = comment_content;
+        this.count_like = count_like;
+        this.is_deprecated = is_deprecated;
+        this.parent_id = parent_id;
+        this.comment_image_id = comment_image_id;
+        this.image_url = image_url;
     }
 
     // For comments child
